62ed1fddccec4e0eb755ed7ec866ce1eb32afddc
ERROR23pl <82032058+ERROR23pl@users.noreply.github.com>
2025-11-29 16:10:59 +0100

    Update prompt handling in generate_start function

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ function generate_start(data) {
   return `<center>
   <a href="https://archiveofourown.org/collections/holofics_shiptober_2025/profile" rel="nofollow">Holofics Shiptober 2025</a>
   
-  <h4>Day ${data.dayNumber}: ${ship(data.charactersA, data.charactersB, data.charactersC)}</h4>
+  <h4>Day ${data.dayNumber}: ${data.prompt}</h4>
+  <h4>${ship(data.charactersA, data.charactersB, data.charactersC)}</h4>
 </center>
 ${data.startNotes}` + generate_ui(data);
 }
@@ -20,6 +21,14 @@ function generate_end(data) {
 function prevDay(data) {
   if (data.dayNumber != 1) {
     return `<td align="left"><a href="${data.prevLink}">⏮️ Day ${data.dayNumber - 1}: ${ship(data.prevCharactersA, data.prevCharactersB, data.prevCharactersC)}</a></td>`
+  } else {
+    return ""
+  }
+}
+
+function prevPrompt(data) {
+  if (data.dayNumber != 1) {
+    return `<td align="left"><a href="${data.prevLink}">(${data.previousPrompt})</a></td>`
   } else {
     return ""
   }
@@ -42,12 +51,28 @@ function nextDay(data) {
   }
 }
 
+function nextPrompt(data) {
+  let nextDayIsOut = (data.nextLink === "");
+  
+  let nextLink = nextDayIsOut ? COLLECTION_LINK : data.nextLink;
+  
+  if (data.dayNumber == 31 || !nextDayIsOut) {
+    return ""
+  }
+  
+  return `<td align="right"><a href="${nextLink}">(${data.nextPrompt})</a></td>`
+}
+
 function generate_ui(data) {
   return `<table>
   <tbody>
     <tr>
       ${prevDay(data)}
       ${nextDay(data)}
+    </tr>
+    <tr>
+      ${prevPrompt(data)}
+      ${nextPrompt(data)}
     </tr>
   </tbody>
 </table>`;
@@ -60,16 +85,19 @@ document.getElementById("generateBtn").addEventListener("click", () => {
     charactersA: document.getElementById("charactersA").value,
     charactersB: document.getElementById("charactersB").value,
     charactersC: document.getElementById("charactersC").value,
+    prompt: document.getElementById("prompt").value,
     
     prevCharactersA: document.getElementById("prevCharactersA").value,
     prevCharactersB: document.getElementById("prevCharactersB").value,
     prevCharactersC: document.getElementById("prevCharactersC").value,
     prevLink: document.getElementById("prevLink").value,
+    previousPrompt: document.getElementById("previousPrompt").value,
     
     nextCharactersA: document.getElementById("nextCharactersA").value,
     nextCharactersB: document.getElementById("nextCharactersB").value,
     nextCharactersC: document.getElementById("nextCharactersC").value,
     nextLink: document.getElementById("nextLink").value,
+    nextPrompt: document.getElementById("nextPrompt").value,
     
     startNotes: document.getElementById("startNotes").value,
     endNotes: document.getElementById("endNotes").value
